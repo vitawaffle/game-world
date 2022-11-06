b174f728fa55088d8ccfd0cdb05f96d4b1e9bbc0
Vitaly Lobatsevich <vitaly.lobatsevich@gmail.com>
2022-11-06 14:06:08 +0300

    Added /users/exists route

--- src/game-world_api/src/Controller/UserController.php
+++ src/game-world_api/src/Controller/UserController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Response, Request};
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\AuthService;
+use App\Repository\UserRepository;
 
 #[Route('/users', name: 'users_')]
 class UserController extends AbstractController
 {
     public function __construct(
         private readonly AuthService $authService,
+        private readonly UserRepository $userRepository,
     ) {
     }
 
@@ -19,5 +21,13 @@ class UserController extends AbstractController
     public function getMe(): Response
     {
         return $this->json($this->authService->getUser());
+    }
+
+    #[Route('/exists', name: 'exists', methods: 'GET')]
+    public function existsBy(Request $request): Response
+    {
+        return $this->json(
+            $this->userRepository->findOneBy($request->query->all()) !== null
+        );
     }
 }
